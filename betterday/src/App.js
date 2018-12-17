14e1cf689e355309692fc524ab1402def5fe08ca
Connor <coyoushaei@gmail.com>
2018-12-17 15:14:49 -0800

    added a username stuff

--- betterday/src/App.js
+++ betterday/src/App.js
@@ -3,23 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    //Instead of the line below, fetch from your database (preferably firebase)
+    this.username = "(insert username here)";
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+           Hello! How has your day been, {this.username}?
         </header>
       </div>
     );
