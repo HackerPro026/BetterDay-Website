6a155edb0eb6d8eb0f268d32f6658b16a1822bec
Connor <coyoushaei@gmail.com>
2018-12-18 08:26:03 -0800

    Added some firebase stuffs

--- betterday/src/App.js
+++ betterday/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import {FIREBASE_CONFIG} from "Components/Firebase/const";
 
 class App extends Component {
 
   constructor(props){
     super(props);
+    firebase.initializeApp(FIREBASE_CONFIG);
     //Instead of the line below, fetch from your database (preferably firebase)
     this.username = "(insert username here)";
   }
---
+++ betterday/src/Components/Firebase/const.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDd4b1QfZoGWSKNSMux5d2Qhj1qruxC7YQ",
+    authDomain: "betterday-firebase.firebaseapp.com",
+    databaseURL: "https://betterday-firebase.firebaseio.com",
+    projectId: "betterday-firebase",
+    storageBucket: "betterday-firebase.appspot.com",
+    messagingSenderId: "762269517020"
+  };
+
