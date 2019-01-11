c190b5b6b256114a517539121956fa2874f44753
Connor <coyoushaei@gmail.com>
2019-01-11 08:47:37 -0800

    firebase added(FINALLY)

--- betterday/src/App.js
+++ betterday/src/App.js
@@ -2,23 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
-import {FIREBASE_CONFIG} from "Components/Firebase/const";
+import {FIREBASE_CONFIG} from "./Components/Firebase/const";
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    firebase.initializeApp(FIREBASE_CONFIG);
+    this.state = {username: ""};
+    if(!firebase.apps.length)
+      firebase.initializeApp(FIREBASE_CONFIG);
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      this.user = result.user;
+
+      this.db = firebase.firestore();
+      this.db.settings({
+        timestampsInSnapshots: true
+      });
+
+      this.db.collection("users").doc(this.user.uid).get().then((doc) => {
+        if(!doc.exists){
+          this.db.collection("users").doc(this.user.uid).set({
+            name: this.user.displayName
+          }).then(() => {
+            console.log("Created account for " + this.user.displayName);
+            window.location.reload();
+          }, (e) => {
+            console.error(e);
+          });
+        }
+        let data = doc.data();
+        this.setState({username: data.name});
+      });
+      
+    }, (e) => {
+      console.error(e);
+    });
+
+    
     //Instead of the line below, fetch from your database (preferably firebase)
-    this.username = "(insert username here)";
+                                    //Here you would fetch the auth
+                                    //UID after you... authenticate
+    
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-           Hello! How has your day been, {this.username}?
+           Hello! How has your day been, {this.state.username}?
         </header>
+        <button onClick={() => {
+          firebase.auth().signOut().then(() => {
+            window.location.reload();
+          });
+        }}>SIGN ME OUT I WANT OUT LET ME OUT AAH</button>
       </div>
     );
   }
--- betterday/src/Components/Firebase/const.js
+++ betterday/src/Components/Firebase/const.js
@@ -1,4 +1,4 @@
-export const FIREBASE_CONFIG = {
+export let FIREBASE_CONFIG = {
     apiKey: "AIzaSyDd4b1QfZoGWSKNSMux5d2Qhj1qruxC7YQ",
     authDomain: "betterday-firebase.firebaseapp.com",
     databaseURL: "https://betterday-firebase.firebaseio.com",
@@ -6,4 +6,3 @@ export const FIREBASE_CONFIG = {
     storageBucket: "betterday-firebase.appspot.com",
     messagingSenderId: "762269517020"
   };
-
